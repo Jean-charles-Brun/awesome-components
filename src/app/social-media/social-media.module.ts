aecf2ae6882db539d8d8a937eb4e7b363b9d1e35
kelerm <kazakaa@gmail.com>
2023-03-27 11:43:34 +0200

    ajout du resolver

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
-import { PostsService } from './services/post.service';
+import { PostsService } from './services/posts.service';
+import { PostsResolver } from './resovers/posts.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     SocialMediaRoutingModule
   ],
   providers: [
-    PostsService
+    PostsService,
+    PostsResolver
   ]
 })
 export class SocialMediaModule { }
