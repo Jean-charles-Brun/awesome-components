b378c42725af55af64608fe858f18c366a9a0308
kelerm <kazakaa@gmail.com>
2023-03-28 11:42:57 +0200

    add: animation depuis "void"

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -24,6 +24,18 @@ import { Comment } from 'src/app/core/models/comment.model';
       ]),
       transition('active => default', [
         animate('500ms ease-in-out')
+      ]),
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          'background-color': 'rgb(201,157,242)',
+        }),
+        animate('250ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+          'background-color': 'white',
+        }))
       ])
     ])
   ]
