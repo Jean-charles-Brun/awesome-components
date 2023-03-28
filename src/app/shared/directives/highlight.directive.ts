6e4765b041b2a5df1adbbe4b0a0a0156256e8fdf
kelerm <kazakaa@gmail.com>
2023-03-28 09:38:57 +0200

    Add: directive highlight

---
+++ src/app/shared/directives/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from "@angular/core";
+
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements AfterViewInit{
+
+  @Input() color = 'yellow';
+
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.setBackGroundColor(this.color);
+  }
+
+  setBackGroundColor(color: string) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+  }
+
+}
