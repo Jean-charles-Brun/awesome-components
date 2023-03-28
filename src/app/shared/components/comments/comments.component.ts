8ad28913abcce5c555756585ae05209f047b62a9
kelerm <kazakaa@gmail.com>
2023-03-28 11:02:48 +0200

    add: animation in trigger

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
@@ -9,7 +9,22 @@ import { Comment } from 'src/app/core/models/comment.model';
   styleUrls: ['./comments.component.scss'],
   animations: [
     trigger('listItem', [
-      
+      state('default', style({
+        trasform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        trasform: 'scale(1.05)',
+        'background-color': 'rgb(201,157,242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ])
     ])
   ]
 })
